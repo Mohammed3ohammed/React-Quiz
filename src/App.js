67c07b551b6fd1c81ab459c724891ccbe3acdf3a
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-04-26 23:16:06 +0300

    Fix reducer typos and implement data fetching in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React from "react";
+import  { useEffect, useReducer } from "react";
 import Header from "./components/Header";
+import Main from "./components/Main";
+
+const initialState = {
+  questions: [],
+
+  status: "loading",
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dateReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+      case "dataFailed": 
+      return {
+        ...state,
+        status: "error",
+      };
+      default: 
+      throw new Error("Action unkonwn")
+  }
+}
 
 
-export default function app() {
+export default function App() {
+const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch(`http://localhost:9000/questions`)
+    .then((res) => res.json())
+    .then((data) => dispatch({type: "dataReceived", payload: data}))
+    .catch((err) => dispatch({type: 'dataFailed'}));
+  }, []);
+
+
   return (
     <div className="app">
       <Header />
+      <Main>
+      <p>1/15</p>
+      <p>Question?</p>
+      </Main>
     </div>
   )
 }
