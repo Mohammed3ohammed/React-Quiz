832d7814ca2775bb03862dff339c394a7fb68ed6
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2026-01-06 22:36:57 +0200

    Implement answer selection and validation logic

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,11 +1,31 @@
-function Options () {
+import { useQuiz } from "../contexts/QuizContext";
+
+function Options ({ question }) {
+  const { dispatch, answer } = useQuiz();
+
+  const hasAnswered = answer !== null;
+
+
   return (
     <div className="options">
-            {question.options.map((option) => (
-                <button className="btn btn-option" key={option}>{option}</button>
+            {question.options.map((option, index) => (
+                <button 
+                   className={`btn btn-option ${index === answer ? "answer" : ""} ${
+                    hasAnswered 
+                    ? index === question.correctOption
+                    ? "correct"
+                    : "wrong"
+                    : ""
+                   } `}
+                      key={option}
+                      disabled={hasAnswered}
+                      onClick={() => dispatch({type: "newAnswer", played: index})}
+                   >
+                      {option}
+                   </button>
             ))}
             </div>
-  )
+  );
 }
 
 export default Options
